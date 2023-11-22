277f07c30dfb1d878b7a28c31d280bee165e7145
Ozoniuss <qscxzsewqazxcde@gmail.com>
2023-11-22 22:00:07 +0200

    feat: update go template

--- aocommon/template.go
+++ aocommon/template.go
@@ -6,20 +6,16 @@ import (
 	"os"
 )
 
-// readPart reads the part number from the standard input.
-func readPart() int {
-	var part int
-	fmt.Print("part: ")
-	fmt.Scanf("%d\n", &part)
-	return part
-}
-
 func main() {
 	f, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-	part := readPart()
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 }
