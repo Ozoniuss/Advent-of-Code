3e5d89207a4ac817190111a138422fd54852b203
Ozoniuss <qscxzsewqazxcde@gmail.com>
2023-12-01 10:35:01 +0200

    feat: improve template and add template for benchmark

--- aocommon/README.md
+++ aocommon/README.md
@@ -5,5 +5,6 @@ This folder contains common items that are useful for the advent of code contest
 ## Table of contents
 
 -   [template.go](template.go): my go template I use when solving the problems
+-   [template_benchmark.go](template.go): my benchmarking template for each part
 -   [get_problem_details_legacy.go](get_problem_details_legacy.py): an old python script I used to convert an HTML problem page into a directory title
 -   [new_problem.sh](new_problem.sh): the script I'm using to generate a folder with some boilerplate for each problem
--- aocommon/template.go
+++ aocommon/template.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 )
 
-func main() {
+// This setup is done not because I like global variables, but in order to avoid
+// any potential system calls during benchmarks (and to also make it easier to
+// just call partXXX function). From my benchmarks it also turns out that s.Text()
+// allocates, even if reusing the variable for the line.
+var inputLines = readlines()
+
+func readlines() []string {
 	f, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+	var lines []string
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		lines = append(lines, s.Text())
 	}
+	return lines
+}
+
+func part1() {
+	for _, line := range inputLines {
+	}
+}
+
+func part2() {
+	for _, line := range inputLines {
+	}
+}
+
+func main() {
+	// Run only 1 profile at a time!
+	// defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
+	// defer profile.Start(profile.MemProfile, profile.ProfilePath("."), profile.MemProfileRate(1)).Stop()
+
+	// This improves speed, but make these functions return something for
+	// benchmarks once problem is solved.
+	part1()
+	part2()
 }
