6f458d1b30d401eb1d55c5c0c974b55a16b764f6
Ozoniuss <qscxzsewqazxcde@gmail.com>
2023-12-02 09:27:16 +0200

    aoc(2023): optimize problem 2 by removing all allocations

--- advent-of-code-2023/02-cube-conundrum/main.go
+++ advent-of-code-2023/02-cube-conundrum/main.go
@@ -29,47 +29,62 @@ func readlines() []string {
 	return lines
 }
 
-func processGame(game string) (int, int, int) {
-	parts := strings.Split(game, ", ")
+func processOneGame(game string) (int, int, int) {
 	red := 0
 	green := 0
 	blue := 0
-	for _, part := range parts {
-		gameWithColor := strings.Split(part, " ")
-		game := gameWithColor[0]
-		color := gameWithColor[1]
+
+	var eoc int
+	for eoc != -1 {
+		var currentGameWithColor string
+		eoc = strings.Index(game, ", ")
+		if eoc == -1 {
+			currentGameWithColor = game
+		} else {
+			currentGameWithColor = game[:eoc]
+			game = game[eoc+2:]
+		}
+
+		space := strings.Index(currentGameWithColor, " ")
+		vals := currentGameWithColor[:space]
+		color := currentGameWithColor[space+1:]
+
 		if color == "red" {
-			val, _ := strconv.Atoi(game)
+			val, _ := strconv.Atoi(vals)
 			red += val
 		}
 		if color == "green" {
-			val, _ := strconv.Atoi(game)
+			val, _ := strconv.Atoi(vals)
 			blue += val
 		}
 		if color == "blue" {
-			val, _ := strconv.Atoi(game)
+			val, _ := strconv.Atoi(vals)
 			green += val
 		}
 	}
 	return red, blue, green
 }
 
-func part1() {
+func part1() int {
 	sum := 0
 	for idx, line := range inputLines {
-		parts := strings.Split(line, ": ")
-		games := parts[1]
-		days := strings.Split(games, "; ")
-		// red := 0
-		// green := 0
-		// blue := 0
+		firstSpace := strings.Index(line, ": ")
+		gamesPart := line[firstSpace+2:]
+
 		possible := false
-		for _, day := range days {
-			redg, greeng, blueg := processGame(day)
-			// red += redg
-			// green += greeng
-			// blue += blueg
-			if redg <= 12 && greeng <= 13 && blueg <= 14 {
+		days := gamesPart
+		eod := 0
+		for eod != -1 {
+			eod = strings.Index(days, "; ")
+			var currentDay string
+			if eod == -1 {
+				currentDay = days
+			} else {
+				currentDay = days[:eod]
+				days = days[eod+2:]
+			}
+			red, green, blue := processOneGame(currentDay)
+			if red <= 12 && green <= 13 && blue <= 14 {
 				possible = true
 			} else {
 				possible = false
@@ -80,19 +95,27 @@ func part1() {
 			sum += idx + 1
 		}
 	}
-	fmt.Println(sum)
+	return sum
 }
-func part2() {
+func part2() int {
 	sum := 0
 	for _, line := range inputLines {
-		parts := strings.Split(line, ": ")
-		games := parts[1]
-		days := strings.Split(games, "; ")
-		red := 0
-		green := 0
-		blue := 0
-		for _, day := range days {
-			redg, greeng, blueg := processGame(day)
+		firstSpace := strings.Index(line, ": ")
+		gamesPart := line[firstSpace+2:]
+
+		days := gamesPart
+		eod := 0
+		var red, green, blue int
+		for eod != -1 {
+			eod = strings.Index(days, "; ")
+			var currentDay string
+			if eod == -1 {
+				currentDay = days
+			} else {
+				currentDay = days[:eod]
+				days = days[eod+2:]
+			}
+			redg, greeng, blueg := processOneGame(currentDay)
 			if redg > red {
 				red = redg
 			}
@@ -105,7 +128,7 @@ func part2() {
 		}
 		sum += red * blue * green
 	}
-	fmt.Println(sum)
+	return sum
 }
 
 func main() {
@@ -119,6 +142,6 @@ func main() {
 	// Part 2 is not written above and commented below so that template compiles
 	// while solving part 1.
 
-	// part1()
-	part2()
+	fmt.Println(part1())
+	fmt.Println(part2())
 }
