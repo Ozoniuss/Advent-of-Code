1895b1fdb003b936824a290ba3199904a66ab3bc
Ozoniuss <qscxzsewqazxcde@gmail.com>
2023-12-03 08:49:52 +0200

    aoc(2023): problem 3

---
+++ advent-of-code-2023/03-gear-ratios/main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"aoc/golib/twod"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// This setup is done not because I like global variables, but in order to avoid
+// any potential system calls during benchmarks (and to also make it easier to
+// just call partXXX function). From my benchmarks it also turns out that s.Text()
+// allocates, even if reusing the variable for the line.
+var inputLines = readlines()
+
+func readlines() []string {
+	f, err := os.Open("padded.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	var lines []string
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		lines = append(lines, s.Text())
+	}
+	return lines
+}
+
+func isNumber(r byte) bool {
+	return r >= '0' && r <= '9'
+}
+
+func isSymbol(r byte) bool {
+	return r != '.' && !isNumber(r)
+}
+
+func part1() {
+	sum := 0
+	for idx, line := range inputLines {
+		current := line + "."
+		inNumber := false
+		var startPos, endPos int
+		var found = false
+		for lineIdx := 0; lineIdx < len(current); lineIdx++ {
+			// currently in number, don't care
+			if isNumber(current[lineIdx]) && inNumber {
+				continue
+			}
+			// currently out of number and not a number, don't care
+			if !isNumber(current[lineIdx]) && !inNumber {
+				continue
+			}
+
+			// reached the beginning of the number, care
+			if isNumber(current[lineIdx]) && !inNumber {
+				inNumber = true
+				startPos = lineIdx
+				continue
+			}
+
+			// reached the end of number, check it
+			if !isNumber(current[lineIdx]) && inNumber {
+				inNumber = false
+				endPos = lineIdx
+			}
+			// fmt.Println(startPos, endPos, currentLine[startPos:endPos])
+			num, err := strconv.Atoi(current[startPos:endPos])
+			if err != nil {
+				panic("strconv")
+			}
+
+			// here work with line not current line
+			// num is startPos->endPos-1
+			for i := idx - 1; i <= idx+1; i++ {
+				for j := startPos - 1; j <= endPos; j++ {
+					if i < 0 || i >= len(inputLines) {
+						continue
+					}
+					if j < 0 || j >= len(line) {
+						continue
+					}
+
+					if i == idx && (j <= endPos-1 && j >= startPos) {
+						continue
+					}
+
+					if isSymbol(inputLines[i][j]) {
+						found = true
+						break
+					}
+				}
+				if found {
+					break
+				}
+			}
+			if found {
+				sum += num
+				fmt.Println("found", num)
+			} else {
+				fmt.Println("not found", num)
+			}
+			startPos, endPos = 0, 0
+			found = false
+		}
+
+	}
+	fmt.Println(sum)
+}
+
+func part2() {
+	positions := make(map[twod.Location][2]int)
+	numberIndex := 0
+	for idx, line := range inputLines {
+		current := line + "."
+		inNumber := false
+		var startPos, endPos int
+		for lineIdx := 0; lineIdx < len(current); lineIdx++ {
+			// currently in number, don't care
+			if isNumber(current[lineIdx]) && inNumber {
+				continue
+			}
+			// currently out of number and not a number, don't care
+			if !isNumber(current[lineIdx]) && !inNumber {
+				continue
+			}
+
+			// reached the beginning of the number, care
+			if isNumber(current[lineIdx]) && !inNumber {
+				inNumber = true
+				startPos = lineIdx
+				continue
+			}
+
+			// reached the end of number, check it
+			if !isNumber(current[lineIdx]) && inNumber {
+				inNumber = false
+				endPos = lineIdx
+			}
+			// fmt.Println(startPos, endPos, currentLine[startPos:endPos])
+			num, err := strconv.Atoi(current[startPos:endPos])
+			if err != nil {
+				panic("strconv")
+			}
+			for i := startPos; i < endPos; i++ {
+				positions[twod.Location{idx, i}] = [2]int{num, numberIndex}
+			}
+			numberIndex++
+
+		}
+
+	}
+	fmt.Println(positions)
+	sum := 0
+	for i := 0; i < len(inputLines); i++ {
+		for j := 0; j < len(inputLines[i]); j++ {
+			prod := 1
+			neighbours := make(map[int]int)
+			if inputLines[i][j] == '*' {
+				for stari := i - 1; stari <= i+1; stari++ {
+					for starj := j - 1; starj <= j+1; starj++ {
+						if val, ok := positions[twod.Location{stari, starj}]; ok {
+							neighbours[val[1]] = val[0]
+						}
+					}
+				}
+				if len(neighbours) == 2 {
+					fmt.Println(neighbours)
+					for _, n := range neighbours {
+						prod = prod * n
+					}
+					fmt.Println("prod", prod)
+					sum = sum + prod
+				}
+			}
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func main() {
+	// Run only 1 profile at a time!
+	// defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
+	// defer profile.Start(profile.MemProfile, profile.ProfilePath("."), profile.MemProfileRate(1)).Stop()
+
+	// No return value improves speed during contest, but make these functions
+	// return something for benchmarks once problem is solved.
+	//
+	// Part 2 is not written above and commented below so that template compiles
+	// while solving part 1.
+
+	// part1()
+	part2()
+}
