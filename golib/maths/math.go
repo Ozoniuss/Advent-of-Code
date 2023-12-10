2ea00e89a1f1ff8d332e35bf906962f96970508a
Ozoniuss <qscxzsewqazxcde@gmail.com>
2023-12-10 12:45:58 +0200

    feat(golib): add GCD and LCM functions

--- golib/maths/math.go
+++ golib/maths/math.go
@@ -8,3 +8,25 @@ func Abs(x int) int {
 		return -x
 	}
 }
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(integers ...int) int {
+	a, b := integers[0], integers[1]
+	result := a * b / GCD(a, b)
+
+	for i := 2; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
