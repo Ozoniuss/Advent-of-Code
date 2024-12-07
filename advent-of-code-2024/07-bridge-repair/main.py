5c7c44fe47f9202f4fe2e684eb55e8ffc764f25a
Ozoniuss <qscxzsewqazxcde@gmail.com>
2024-12-07 10:45:59 +0200

    aoc(2024): use log instead of string concat
    It's not really more efficient

--- advent-of-code-2024/07-bridge-repair/main.py
+++ advent-of-code-2024/07-bridge-repair/main.py
@@ -24,8 +24,16 @@ def solve(left, right, current):
     )
 
 
+def log(n):
+    c = 0
+    while n > 0:
+        n = n // 10
+        c += 1
+    return c
+
+
 def solve_2(left, right, current):
-    print(left, right, current)
+    # print(left, right, current)
     if current > left:
         return 0
 
@@ -35,22 +43,24 @@ def solve_2(left, right, current):
     if len(right) == 0 and current != left:
         return 0
 
+    # concat = current * (10 ** log(right[0])) + right[0]
+    concat = int(str(current) + str(right[0]))
     return (
         solve_2(left, right[1:], current * right[0])
         + solve_2(left, right[1:], current + right[0])
-        + solve_2(left, right[1:], int(str(current) + str(right[0])))
+        + solve_2(left, right[1:], concat)
     )
 
 
 t = 0
-for line in lines:
+for idx, line in enumerate(lines):
     parts = line.split(":")
     left = int(parts[0])
     right = list(map(int, parts[1].strip().split(" ")))
 
-    # if solve(left, right[1:], right[0]) > 0:
-    # t += left
     if solve_2(left, right[1:], right[0]) > 0:
         t += left
+
+    print(idx)
 
 print(t)
